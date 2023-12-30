696330373c9691156b2525a921a32a5b45c9f64d
Rajibul Islam <rajibulski110@gmail.com>
2023-12-30 14:52:39 +0530

    first commit: mongodb and express setup

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import connectDB from "./db/connect.js";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening to prot ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
