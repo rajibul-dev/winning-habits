d03e1aa3be5823b9310b7602ea7adf487a0edf55
Rajibul Islam <rajibulski110@gmail.com>
2024-01-02 22:40:53 +0530

    official app gmail app handles the email sending

--- backend/utils/nodeMailer/nodemailerConfig.js
+++ backend/utils/nodeMailer/nodemailerConfig.js
@@ -1,9 +1,12 @@
+import { config } from "dotenv";
+config();
+
 export default {
   host: "smtp.gmail.com",
   port: 587,
   secure: false, // `true` for port 465, `false` for all other ports
   auth: {
-    user: "winninghabitapp@gmail.com",
-    pass: "kfey apoz xubr izas",
+    user: process.env.APP_EMAIL,
+    pass: process.env.APP_EMAIL_PASS,
   },
 };
--- backend/utils/nodeMailer/sendVerificationToken.js
+++ backend/utils/nodeMailer/sendVerificationToken.js
@@ -8,8 +8,6 @@ export default async function sendVerificationToken({
 }) {
   const verifyEmailLink = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`;
 
-  console.log(name);
-
   const message = `<p>Hello ${name}, please confirm your email by clicking on the following link: <a href="${verifyEmailLink}">${verifyEmailLink}</a></p>`;
 
   sendEmail({
