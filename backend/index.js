d9ac6e372a20eadef0830a44713e4239cd0836b4
Rajibul Islam <rajibulski110@gmail.com>
2024-01-06 01:59:51 +0530

    added cookieParser

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import "express-async-errors";
 import express from "express";
 const app = express();
 
+import cookieParser from "cookie-parser";
+
 import connectDB from "./db/connect.js";
 import notFoundMiddleware from "./middlewares/notFoundMiddleware.js";
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
@@ -12,6 +14,7 @@ import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
 import authRouter from "./routes/authRoutes.js";
 
 app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
 
 app.get("/", (req, res) => {
   res.send(`<h1>Winning Habits API</h1>`);
