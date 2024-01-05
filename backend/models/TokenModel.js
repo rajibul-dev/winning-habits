ff7262e3ad3bbe8b083d1fbbeef0a05bfd2946fc
Rajibul Islam <rajibulski110@gmail.com>
2024-01-05 19:16:48 +0530

    Token model created

---
+++ backend/models/TokenModel.js
@@ -0,0 +1,14 @@
+import { Schema, model, Types } from "mongoose";
+
+const TokenSchema = new Schema(
+  {
+    refreshToken: { type: String, required: true },
+    ip: { type: String, required: true },
+    userAgent: { type: String, required: true },
+    isValid: { type: Boolean, default: true },
+    user: { type: Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true },
+);
+
+export default model("Token", TokenSchema);
