02ceb0a067576ae4c441e8b05b585267d8b8b4ad
Rajibul Islam <rajibulski110@gmail.com>
2024-01-13 20:40:41 +0530

    Bettering habit model

--- backend/models/HabitModel.js
+++ backend/models/HabitModel.js
@@ -12,12 +12,12 @@ const HabitSchema = new Schema(
           type: String,
           enum: ["yes", "no", "answered"],
         },
-        points: Number,
+        points: { type: Number, default: 0 },
         date: { type: Date, default: Date.now },
       },
     ],
-    streak: Number,
-    totalPoints: Number,
+    streak: { type: Number, default: 0 },
+    totalPoints: { type: Number, default: 0 },
     user: {
       type: mongoose.Types.ObjectId,
       ref: "User",
@@ -32,15 +32,22 @@ HabitSchema.pre("save", function (next) {
   if (this.isModified("dailyRecords")) {
     const latestRecord = this.dailyRecords[this.dailyRecords.length - 1];
 
-    // if we choose 'yes' in the habit
-    if (latestRecord && latestRecord.didIt === "yes") {
-      this.streak = (this.streak || 0) + 1;
-      latestRecord.points = this.streak;
-      this.totalPoints = (this.totalPoints || 0) + latestRecord.points;
-    } else {
-      // if the answer was 'no', or 'unanswered' in the habit
-      this.streak = 0;
-      latestRecord.points = this.streak;
+    if (latestRecord) {
+      // if we choose 'yes' in the habit
+      if (latestRecord.didIt === "yes") {
+        this.streak = (this.streak || 0) + 1;
+        latestRecord.points = this.streak;
+        this.totalPoints = (this.totalPoints || 0) + latestRecord.points;
+      }
+      // if it was 'unanswered' in the habit
+      else if (latestRecord.didIt === "unanswered") {
+        next();
+      }
+      // if the answer was 'no' in the habit
+      else {
+        this.streak = 0;
+        latestRecord.points = this.streak;
+      }
     }
   }
 
