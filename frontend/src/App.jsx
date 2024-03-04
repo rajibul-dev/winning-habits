f83408aaaef7f136b5886561d0f6d8d7cc07efaf
Rajibul Islam <rajibulski110@gmail.com>
2024-03-04 22:34:11 +0530

    frontend packages are installed, frontend project initialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,9 @@
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div className="flex h-dvh items-center justify-center">
+      <h1 className=" text-center text-9xl font-bold text-yellow-400">
+        Hello world!
+      </h1>
+    </div>
+  );
 }
