25ec7cc547192e029f3fafbb3e3df2f6cfc16f77
Rajibul Islam <rajibulski110@gmail.com>
2024-03-11 19:24:57 +0530

    gave up on tailwind and installed styled components

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
-  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
