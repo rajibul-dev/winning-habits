24ceff7caff4ce46aac8b2095d3bad3845de7715
Rajibul Islam <rajibulski110@gmail.com>
2024-03-13 03:48:14 +0530

    manual show hide password input

---
+++ frontend/src/ui/InputPasswordShowHide.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  font-size: 1.6rem;
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
+  padding-right: 6rem;
+`;
+
+const ToggleButton = styled.button`
+  position: absolute;
+  right: 1.5rem;
+  top: 50%;
+  transform: translateY(-50%);
+
+  background: none;
+  border: none;
+  color: var(--color-grey-500);
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default function InputPasswordShowHide({ id, ...rest }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <Wrapper>
+      <Input id={id} type={!showPassword ? "password" : "name"} {...rest} />
+      <ToggleButton
+        onClick={(e) => {
+          e.preventDefault();
+          setShowPassword((curr) => !curr);
+        }}
+      >
+        {!showPassword ? "Show" : "Hide"}
+      </ToggleButton>
+    </Wrapper>
+  );
+}
