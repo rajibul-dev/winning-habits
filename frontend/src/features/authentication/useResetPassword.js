8d00d50edf5bd52d2fe12009d22c674bdf48fa0e
Rajibul Islam <rajibulski110@gmail.com>
2024-03-14 15:53:59 +0530

    nothing really

--- frontend/src/features/authentication/useResetPassword.js
+++ frontend/src/features/authentication/useResetPassword.js
@@ -1,12 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
 import { resetPassword as apiResetPassword } from "../../api/apiAuth.js";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export default function useResetPassword() {
+  const navigate = useNavigate();
   const {
     mutate: resetPassword,
     isPending: isResetting,
     isSuccess,
-    error
+    error,
   } = useMutation({
     mutationFn: ({ email, token, password }) =>
       apiResetPassword({ email, token, password }),
