88f68543700a765424faab4a37d1bf1849c7ef99
Rajibul Islam <rajibulski110@gmail.com>
2024-03-14 15:52:16 +0530

    "If no account then create an account" link to register page

--- frontend/src/features/authentication/LoginForm.jsx
+++ frontend/src/features/authentication/LoginForm.jsx
@@ -26,47 +26,61 @@ export default function LoginForm() {
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      {error && (
-        <FormRowVertical>
-          <PageLevelNotificationToast type="error">
-            {error.response.data.msg}
-          </PageLevelNotificationToast>
+    <>
+      <Form onSubmit={handleSubmit}>
+        {error && (
+          <FormRowVertical>
+            <PageLevelNotificationToast type="error">
+              {error.response.data.msg}
+            </PageLevelNotificationToast>
+          </FormRowVertical>
+        )}
+        <FormRowVertical label="Email address">
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            // This makes this form better for password managers
+            autoComplete="username"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </FormRowVertical>
-      )}
-      <FormRowVertical label="Email address">
-        <Input
-          type="email"
-          id="email"
-          name="email"
-          // This makes this form better for password managers
-          autoComplete="username"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </FormRowVertical>
 
-      <FormRowVertical label="Password">
-        <Input
-          type="password"
-          id="password"
-          name="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </FormRowVertical>
+        <FormRowVertical label="Password">
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </FormRowVertical>
 
-      <FormRowVertical>
-        <Button disabled={isLogining}>
-          {!isLogining ? "Login" : "Logging in..."}
-        </Button>
-      </FormRowVertical>
-      <InlineLink to="/forgot-password" usage="forgot-password">
-        Forgot password?
-      </InlineLink>
-    </Form>
+        <FormRowVertical>
+          <Button disabled={isLogining}>
+            {!isLogining ? "Login" : "Logging in..."}
+          </Button>
+        </FormRowVertical>
+        <InlineLink to="/forgot-password" usage="forgot-password">
+          Forgot password?
+        </InlineLink>
+      </Form>
+      <p
+        style={{
+          textAlign: "center",
+          marginTop: "-1.4rem",
+          fontSize: "1.6rem",
+        }}
+      >
+        Don't have an account?&nbsp;&nbsp;
+        <InlineLink to="/register" usage="pale-color">
+          Create an account
+        </InlineLink>
+      </p>
+    </>
   );
 }
