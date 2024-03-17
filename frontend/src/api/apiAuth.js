3a2d7ef7fc32e704c1754a2563490139cda77d80
Rajibul Islam <rajibulski110@gmail.com>
2024-03-17 15:25:08 +0530

    better way of exporting in this case

--- frontend/src/api/apiAuth.js
+++ frontend/src/api/apiAuth.js
@@ -1,7 +1,8 @@
 import apiClient from "./axiosConfig";
+import { endpointV1 } from "./axiosConfig.js";
 
-async function register({ name, email, password }) {
-  const res = await apiClient.post("/api/v1/auth/register", {
+export async function register({ name, email, password }) {
+  const res = await apiClient.post(`${endpointV1}/auth/register`, {
     name,
     email,
     password,
@@ -9,49 +10,49 @@ async function register({ name, email, password }) {
   return res.data;
 }
 
-async function verifyEmail({ verificationToken, email }) {
-  const res = await apiClient.post("/api/v1/auth/verify-email", {
+export async function verifyEmail({ verificationToken, email }) {
+  const res = await apiClient.post(`${endpointV1}/auth/verify-email`, {
     email,
     verificationToken,
   });
   return res.data;
 }
 
-async function login({ email, password }) {
-  const res = await apiClient.post("/api/v1/auth/login", {
+export async function login({ email, password }) {
+  const res = await apiClient.post(`${endpointV1}/auth/login`, {
     email,
     password,
   });
   return res.data;
 }
 
-async function getCurrentUser() {
-  const res = await apiClient.get("/api/v1/users/showMe");
+export async function getCurrentUser() {
+  const res = await apiClient.get(`${endpointV1}/users/showMe`);
   return res.data;
 }
 
-async function logout() {
-  const res = await apiClient.get("/api/v1/auth/logout");
+export async function logout() {
+  const res = await apiClient.get(`${endpointV1}/auth/logout`);
   return res.data;
 }
 
-async function requestNewVerificationEmail(email) {
+export async function requestNewVerificationEmail(email) {
   const res = await apiClient.post(
-    "/api/v1/auth/request-new-verification-email",
+    `${endpointV1}/auth/request-new-verification-email`,
     { email },
   );
   return res.data;
 }
 
-async function forgotPassword(email) {
-  const res = await apiClient.post("/api/v1/auth/forgot-password", {
+export async function forgotPassword(email) {
+  const res = await apiClient.post(`${endpointV1}/auth/forgot-password`, {
     email,
   });
   return res.data;
 }
 
-async function resetPassword({ email, token, password }) {
-  const res = await apiClient.post("/api/v1/auth/reset-password", {
+export async function resetPassword({ email, token, password }) {
+  const res = await apiClient.post(`${endpointV1}/auth/reset-password`, {
     email,
     token,
     password,
@@ -59,22 +60,10 @@ async function resetPassword({ email, token, password }) {
   return res.data;
 }
 
-async function changePassword({ currentPassword, newPassword }) {
-  const res = await apiClient.post("/api/v1/auth/change-password", {
+export async function changePassword({ currentPassword, newPassword }) {
+  const res = await apiClient.post(`${endpointV1}/auth/change-password`, {
     currentPassword,
     newPassword,
   });
   return res.data;
 }
-
-export {
-  register,
-  verifyEmail,
-  login,
-  getCurrentUser,
-  logout,
-  requestNewVerificationEmail,
-  forgotPassword,
-  resetPassword,
-  changePassword,
-};
