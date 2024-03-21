05d57574baab3c85e61121c6c15b7bd194ea68ae
Rajibul Islam <rajibulski110@gmail.com>
2024-03-21 23:29:27 +0530

    commneded unused styles

--- frontend/src/features/authentication/UserAvatar.jsx
+++ frontend/src/features/authentication/UserAvatar.jsx
@@ -10,16 +10,16 @@ const StyledUserAvatar = styled.div`
   color: var(--color-grey-600);
 `;
 
-const Avatar = styled.img`
-  display: block;
-  width: 4rem;
-  width: 3.6rem;
-  aspect-ratio: 1;
-  object-fit: cover;
-  object-position: center;
-  border-radius: 50%;
-  outline: 2px solid var(--color-grey-100);
-`;
+// const Avatar = styled.img`
+//   display: block;
+//   width: 4rem;
+//   width: 3.6rem;
+//   aspect-ratio: 1;
+//   object-fit: cover;
+//   object-position: center;
+//   border-radius: 50%;
+//   outline: 2px solid var(--color-grey-100);
+// `;
 
 function UserAvatar() {
   const { user } = useUser();
