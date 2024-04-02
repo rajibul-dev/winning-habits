d171f40b65208f2d7ed91c11291af08889ac21f7
Rajibul Islam <rajibulski110@gmail.com>
2024-04-02 17:12:38 +0530

    removed backdrop blur from overlay

--- frontend/src/ui/Modal.jsx
+++ frontend/src/ui/Modal.jsx
@@ -30,7 +30,7 @@ const Overlay = styled.div`
   width: 100%;
   height: 100vh;
   background-color: var(--backdrop-color);
-  backdrop-filter: blur(4px);
+
   z-index: 1000;
   transition: all 0.5s;
 `;
