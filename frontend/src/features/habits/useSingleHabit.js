fcc0188b1e763d985848df630d03ad3c5a488477
Rajibul Islam <rajibulski110@gmail.com>
2024-04-07 22:47:44 +0530

    fixed query key

--- frontend/src/features/habits/useSingleHabit.js
+++ frontend/src/features/habits/useSingleHabit.js
@@ -1,14 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { getSingleHabit } from "../../api/apiHabits.js";
+import { useSearchParams } from "react-router-dom";
 
 export default function useSingleHabit() {
+  const { id } = useSearchParams();
+
   const {
     data: habit,
     isLoading,
     error,
   } = useQuery({
     queryFn: getSingleHabit,
-    queryKey: ["single-habit"],
+    queryKey: ["habit", id],
   });
+
   return { habit, isLoading, error };
 }
