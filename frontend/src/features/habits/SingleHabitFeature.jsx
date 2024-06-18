233d04b5d9ee471e469b21fe22be254a793a9b04
Rajibul Islam <rajibulski110@gmail.com>
2024-06-18 20:41:14 +0530

    back button on the heading

--- frontend/src/features/habits/SingleHabitFeature.jsx
+++ frontend/src/features/habits/SingleHabitFeature.jsx
@@ -1,15 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Empty from "../../ui/Empty.jsx";
 import Heading from "../../ui/Heading.jsx";
 import Spinner from "../../ui/Spinner.jsx";
 import useSingleHabit from "./useSingleHabit.js";
 import styled from "styled-components";
 import Button from "../../ui/Button.jsx";
+import { IoArrowBack } from "react-icons/io5";
 
 const GoBackLink = styled(Link)``;
+const StyledHeading = styled(Heading)`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  & svg {
+    display: inline-block;
+    width: 3.4rem;
+    height: 3.4rem;
+    cursor: pointer;
+    &:hover {
+      background-color: var(--color-grey-200);
+    }
+  }
+`;
 
 export default function SingleHabitFeature() {
   const { data, isLoading, error } = useSingleHabit();
+  const navigate = useNavigate();
 
   if (isLoading) return <Spinner />;
   if (error)
@@ -26,7 +43,10 @@ export default function SingleHabitFeature() {
 
   return (
     <>
-      <Heading>{name}</Heading>
+      <StyledHeading>
+        <IoArrowBack onClick={() => navigate(-1)} />
+        {name}
+      </StyledHeading>
     </>
   );
 }
