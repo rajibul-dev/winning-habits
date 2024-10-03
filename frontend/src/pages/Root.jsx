786c14fc986ce3ab9783f450880420956e8a002a
Rajibul Islam <rajibulski110@gmail.com>
2024-10-03 14:49:33 +0530

    Landing page done (just a make do version)

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,3 +1,102 @@
+import styled from "styled-components";
+import Logo from "../ui/Logo";
+import Heading from "../ui/Heading";
+import Button from "../ui/Button";
+import DarkModeToggleBtn from "../ui/DarkModeToggleBtn";
+import { useNavigate } from "react-router-dom";
+import { pixelToEm } from "../styles/GlobalStyles";
+
+const Section = styled.div`
+  padding: 6rem 0;
+`;
+
+const HeaderSection = styled(Section)`
+  padding: 3rem 0;
+`;
+
+const Container = styled.div`
+  max-width: 130rem;
+  margin-inline: auto;
+  padding: 0 3rem;
+`;
+
+const Header = styled(Container)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > div {
+    text-align: start;
+  }
+
+  & button svg {
+    width: 4rem;
+    height: 4rem;
+  }
+`;
+
+const HeroHeading = styled(Heading)`
+  font-size: 4.4rem;
+  font-weight: 800;
+  max-width: 22ch;
+  text-align: center;
+  display: table;
+  margin-inline: auto;
+  margin-top: 2rem;
+  margin-bottom: 3rem;
+
+  @media (max-width: ${pixelToEm(634)}) {
+    margin-top: 6rem;
+    font-size: 3.6rem;
+  }
+
+  @media (max-width: ${pixelToEm(477)}) {
+    font-size: 3rem;
+  }
+
+  @media (max-width: ${pixelToEm(375)}) {
+    font-size: 2.6rem;
+  }
+`;
+
+const HeroButton = styled(Button)`
+  margin-inline: auto;
+  display: table;
+  padding-inline: 6rem;
+
+  @media (max-width: ${pixelToEm(477)}) {
+    font-size: 1.6rem;
+    display: block;
+    width: 100%;
+  }
+
+  @media (max-width: ${pixelToEm(375)}) {
+    font-size: 1.6rem;
+  }
+`;
+
 export default function Root() {
-  return <div className="root">Root</div>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <HeaderSection>
+        <Header>
+          <Logo />
+          <DarkModeToggleBtn />
+        </Header>
+      </HeaderSection>
+
+      <Section>
+        <Container>
+          <HeroHeading as="h1">
+            Track your habits with Winning Habits
+          </HeroHeading>
+          <HeroButton onClick={() => navigate("/app")} size="large">
+            Open app
+          </HeroButton>
+        </Container>
+      </Section>
+    </>
+  );
 }
