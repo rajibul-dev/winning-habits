f72d5303f2a794da2df2e5d2c17ca70bda7d05fe
Rajibul Islam <rajibulski110@gmail.com>
2024-10-03 14:49:09 +0530

    logo will navigate to the root page

--- frontend/src/ui/Logo.jsx
+++ frontend/src/ui/Logo.jsx
@@ -2,10 +2,12 @@ import styled from "styled-components";
 import LogoSVG from "../assets/logo-light-mode.svg";
 import LogoSVGDark from "../assets/logo-dark-mode.svg";
 import { useDarkMode } from "../context/DarkModeContext";
+import { useNavigate } from "react-router-dom";
 
 const StyledLogo = styled.div`
   text-align: center;
   line-height: 0;
+  cursor: pointer;
 `;
 
 const Img = styled.img`
@@ -13,11 +15,12 @@ const Img = styled.img`
 `;
 
 export default function Logo() {
+  const navigate = useNavigate();
   const { isDarkMode } = useDarkMode();
   const src = isDarkMode ? LogoSVGDark : LogoSVG;
 
   return (
-    <StyledLogo>
+    <StyledLogo onClick={() => navigate("/")}>
       <Img src={src} alt="winning habits logo" />
     </StyledLogo>
   );
