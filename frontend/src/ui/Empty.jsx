d74806ba6a6e3a2f89be0c4280f40104a70817c2
Rajibul Islam <rajibulski110@gmail.com>
2024-10-08 02:57:59 +0530

    bigger font for empty resources

--- frontend/src/ui/Empty.jsx
+++ frontend/src/ui/Empty.jsx
@@ -1,5 +1,11 @@
+import styled from "styled-components";
+
+const Paragraph = styled.p`
+  font-size: var(--font-size-base);
+`;
+
 function Empty({ resourceName }) {
-  return <p>No {resourceName} could be found.</p>;
+  return <Paragraph>No {resourceName} could be found.</Paragraph>;
 }
 
 export default Empty;
