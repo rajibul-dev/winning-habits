1f50e9f04b10cc71740240ca7bf1d69e7859005d
Rajibul Islam <rajibulski110@gmail.com>
2024-10-08 03:12:06 +0530

    well, let's believe we are handling errors with error boundaries

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,20 @@ import App from "./App.jsx";
 import { PopoverManagerProvider } from "./ui/Popover.jsx";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "./store.js";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <PopoverManagerProvider>
-        <App />
-      </PopoverManagerProvider>
-    </ReduxProvider>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
+      <ReduxProvider store={store}>
+        <PopoverManagerProvider>
+          <App />
+        </PopoverManagerProvider>
+      </ReduxProvider>
+    </ErrorBoundary>
   </React.StrictMode>,
 );
