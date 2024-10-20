d798d873ef2bea5818ee0f28cf2ce7ca6f3a5ba5
Rajibul Islam <rajibulski110@gmail.com>
2024-10-20 14:53:42 +0530

    winning habits header on forgot password and reset password pages

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,12 +1,16 @@
 import ForgotPasswordForm from "../features/authentication/ForgotPasswordForm.jsx";
 import Heading from "../ui/Heading.jsx";
+import NoAccountHeader from "../ui/NoAccountHeader.jsx";
 import { LoginLayout } from "./Login.jsx";
 
 export default function ForgotPassword() {
   return (
-    <LoginLayout>
-      <Heading as="h4">Forgot password?</Heading>
-      <ForgotPasswordForm />
-    </LoginLayout>
+    <>
+      <NoAccountHeader />
+      <LoginLayout>
+        <Heading as="h4">Forgot password?</Heading>
+        <ForgotPasswordForm />
+      </LoginLayout>
+    </>
   );
 }
--- frontend/src/pages/ResetPassword.jsx
+++ frontend/src/pages/ResetPassword.jsx
@@ -1,12 +1,16 @@
 import ResetPasswordForm from "../features/authentication/ResetPasswordForm.jsx";
 import Heading from "../ui/Heading.jsx";
+import NoAccountHeader from "../ui/NoAccountHeader.jsx";
 import { LoginLayout as ResetPasswordLayout } from "./Login.jsx";
 
 export default function ResetPassword() {
   return (
-    <ResetPasswordLayout $page="reset-password">
-      <Heading as="h4">Create new password</Heading>
-      <ResetPasswordForm />
-    </ResetPasswordLayout>
+    <>
+      <NoAccountHeader />
+      <ResetPasswordLayout $page="reset-password">
+        <Heading as="h4">Create new password</Heading>
+        <ResetPasswordForm />
+      </ResetPasswordLayout>
+    </>
   );
 }
